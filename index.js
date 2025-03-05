399badb064380faf8e82a5a1d8ffdf3bece0d4ca
m4rcin7 <mrku1337@gmail.com>
2025-03-05 09:20:48 +0100

    :bot

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const PORT = process.env.PORT || 4040;
+
+const app = express();
+app.use(express.json());
+
+app.post("*", (req, res) => {
+  res.send("Hello post!");
+});
+
+app.get("*", (req, res) => {
+  res.send("Hello get!");
+});
+
+app.listen(PORT, () => {
+  if (err) console.log(err);
+  console.log(`Server listening on PORT ${PORT}`, PORT);
+});
