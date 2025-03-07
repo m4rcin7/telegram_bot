c0654a4fe6d171ad8adcdb228ef5e694abad91f2
m4rcin7 <mrku1337@gmail.com>
2025-03-07 18:26:08 +0100

    :bot

--- controller/lib/axios.js
+++ controller/lib/axios.js
@@ -4,8 +4,20 @@ const MY_TOKEN = process.env.MY_TOKEN;
 const BASE_URL = `https://api.telegram.org/bot${MY_TOKEN}`;
 
 function getAxiosInstance() {
-  return axios.get(`/${method}`, {
-    baseURL: BASE_URL,
-    params,
-  });
+  return {
+    get(method, params) {
+      return axios.get(`/${method}`, {
+        baseURL: BASE_URL,
+        params,
+      });
+    },
+    post(method, data) {
+      return axios({
+        method: "post",
+        baseURL: BASE_URL,
+        url: `/${method}`,
+        data,
+      });
+    },
+  };
 }
