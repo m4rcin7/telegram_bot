627cb74b7aa5397916c1fe7a9271f9fa89800bb1
m4rcin7 <mrku1337@gmail.com>
2025-03-07 18:39:49 +0100

    :bot

--- controller/lib/Telegram.js
+++ controller/lib/Telegram.js
@@ -1,4 +1,4 @@
-const {axiosInstance} = require("./axios");
+const { axiosInstance } = require("./axios");
 
 function sendMessage(messageObj, messageText) {
   return axiosInstance.get("sendMessage", {
@@ -6,3 +6,24 @@ function sendMessage(messageObj, messageText) {
     text: messageText,
   });
 }
+
+function handleMessage(messageObj) {
+  const messageText = messageObj.text || "";
+
+  if (messageText.chatAt(0) === "/") {
+    const command = messageText.substr(1);
+    switch (command) {
+      case "start":
+        return sendMessage(
+          messageObj,
+          "Hi! I'm a bot. I can help you to get started!"
+        );
+      default:
+        return sendMessage(messageObj, "Hey! I don't understand that command.");  
+    }
+  } else {
+    return sendMessage(messageObj, messageText);
+  }
+}
+
+module.exports = { handleMessage };
