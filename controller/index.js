290d2b33fc0a1d96d60925b8f9e7084a9beb55c4
m4rcin7 <mrku1337@gmail.com>
2025-03-07 18:47:05 +0100

    :bot

--- controller/index.js
+++ controller/index.js
@@ -1,6 +1,7 @@
 const { handleMessage } = require("./lib/Telegram");
 
 async function handle(req, res) {
+  const { body } = req;
   if (body) {
     const messageObj = body.message;
     await handleMessage(messageObj);
--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.post("*", async (req, res) => {
   res.send(await handler(req));
 });
 
-app.get("*", (req, res) => {
-  res.send("Hello get!");
+app.get("*", async (req, res) => {
+  res.send(await handler(req));
 });
 
 app.listen(PORT, function (err) {
   if (err) console.log(err);
-  console.log(`Server listening on PORT ${PORT}`, PORT);
+  console.log(`Server listening on PORT`, PORT);
 });
