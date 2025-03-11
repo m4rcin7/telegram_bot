d89b78b47135bee057cd63f5b61045587a5de2b6
m4rcin7 <mrku1337@gmail.com>
2025-03-11 17:24:56 +0100

    :bot

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const { handle } = require("./controller/lib/Telegram");
 
-const PORT = process.env.PORT || 4040;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
